82e5624c373d1ac0fbe2179d77203fd3c77efe55
GnKonkort <20600280+GnKonkort@users.noreply.github.com>
2025-11-12 00:17:25 +0300

    Synchronization of component between client and server

--- Content.Server/_CitadelStation/Woundmed/Systems/TargetingSystem.cs
+++ Content.Server/_CitadelStation/Woundmed/Systems/TargetingSystem.cs
@@ -1,10 +1,12 @@
 using Content.Shared._CitadelStation.WoundMed.Events;
+using Content.Shared._CitadelStation.WoundMed.Components;
 
 namespace Content.Server._CitadelStation.Woundmed.Systems;
 
 
 public sealed class TargetingSystem : EntitySystem
 {
+    [Dependency] private readonly IEntityManager _entManager = default!;
     public override void Initialize(){
         base.Initialize();
         SubscribeNetworkEvent<BodypartTargetChangeEvent>(OnBodypartTargetChange);
@@ -12,6 +14,13 @@ public sealed class TargetingSystem : EntitySystem
 
     private void OnBodypartTargetChange(BodypartTargetChangeEvent ev)
     {
+        var user = GetEntity(ev.Uid);
+        if(_entManager.GetComponent<BodypartTargetingComponent>(user) is not { } bodypartTargetingComponent
+            || ev.Bodypart == bodypartTargetingComponent.chosenBodyPart) {
+            return;
+        }
+        bodypartTargetingComponent.chosenBodyPart = ev.Bodypart;
         Logger.Debug($"Someone changed targetbodypart {ev.Uid} - {ev.Bodypart}");
+        Dirty(GetEntity(ev.Uid),bodypartTargetingComponent);
     }
 }
